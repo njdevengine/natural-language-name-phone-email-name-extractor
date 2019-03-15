987c3c1409fb8451eedf6d9d91287036903bf3e2
njdevengine <newjerseydevelopment@gmail.com>
2019-03-15 16:43:55 -0400

    Create nltk-by-text-file.py

---
+++ nltk-by-text-file.py
@@ -0,0 +1,50 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+stop = stopwords.words('english')
+
+###FILE HERE###
+
+with open('text_list.txt', 'r') as myfile:
+    string=myfile.read().replace('\n', '')
+
+def extract_phone_numbers(string):
+    r = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+    phone_numbers = r.findall(string)
+    return [re.sub(r'\D', '', number) for number in phone_numbers]
+
+def extract_email_addresses(string):
+    r = re.compile(r'[\w\.-]+@[\w\.-]+')
+    return r.findall(string)
+
+def ie_preprocess(document):
+    document = ' '.join([i for i in document.split() if i not in stop])
+    sentences = nltk.sent_tokenize(document)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
+    return sentences
+
+def extract_names(document):
+    names = []
+    sentences = ie_preprocess(document)
+    for tagged_sentence in sentences:
+        for chunk in nltk.ne_chunk(tagged_sentence):
+            if type(chunk) == nltk.tree.Tree:
+                if chunk.label() == 'PERSON':
+                    names.append(' '.join([c[0] for c in chunk]))
+    return names
+
+if __name__ == '__main__':
+    numbers = extract_phone_numbers(string)
+    emails = extract_email_addresses(string)
+    names = extract_names(string)
+
+print(numbers)
+print(emails)
+print(names)
